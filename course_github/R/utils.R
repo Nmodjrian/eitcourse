c8724dd78faf8c40b010aec3201cbd92126271d5
Newsha Modjrian <nmodjrian@gmail.com>
2025-09-08 16:46:39 +0300

    Remove all files from the repository

--- course_github/R/utils.R
+++
@@ -1,8 +0,0 @@
-#' Course Utilities
-#'
-#' A collection of utility functions for the course.
-#'
-#' @export
-hello_course <- function() {
-  message("Welcome to Tools and Skills for Reproducible Transport Research!")
-}
